edc7e12cf4a1f09a9a60d94f0cbf228cbe2dc41f
Alexei Vinogradov <6568032+vinogradoff@users.noreply.github.com>
2022-06-28 22:11:52 +0200

    update

--- src/test/java/de/vinogradovit/performance/SimpleTest.java
+++ src/test/java/de/vinogradovit/performance/SimpleTest.java
@@ -1,9 +1,11 @@
 package de.vinogradovit.performance;
 
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.*;
 import io.cloudbeat.junit.CbJunitExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.net.MalformedURLException;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
@@ -12,9 +14,22 @@ import static com.codeborne.selenide.Selenide.*;
 public class SimpleTest {
 
   @Test
-  void findSelenideOnGithub(){
+  void findSelenideOnGithub() throws MalformedURLException {
+    //var driver=CbJunitExtension.createWebDriver();
+
+    //WebDriverRunner.setWebDriver(driver);
     open("https://github.com");
     $("abc").shouldBe(visible);
+
+  }
+
+  @Test
+  void workingTest() throws MalformedURLException {
+    var driver=CbJunitExtension.createWebDriver();
+
+    WebDriverRunner.setWebDriver(driver);
+    open("https://github.com");
+    $("div").shouldBe(visible);
 
   }
 }
